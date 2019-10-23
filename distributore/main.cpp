998ae59ed4e3aaa4583bcc288918e7de154570c1
Martin Masevski <martin.masevski@gmail.com>
2019-10-23 11:49:52 +0200

    1st menu sample

---
+++ distributore/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+void clear()
+{
+#if defined _WIN32
+    system("cls");
+#elif defined (__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
+    system("clear");
+#elif defined (__APPLE__)
+    system("clear");
+#endif
+}
+
+void benvenuto() {
+    std::cout << "  -------------------------\n";
+    std::cout << " |Il Distributore di Martin|\n";
+    std::cout << "  -------------------------\n";
+}
+
+void displayMenu() {
+    std::cout << "\n";
+    std::cout << " ============================ \n";
+    std::cout << " | Comandi disponibili:     |\n";
+    std::cout << " |                          |\n";
+    std::cout << " | 1) Fai cosa A            |\n";
+    std::cout << " | 2) Fai cosa B            |\n";
+    std::cout << " | 3) Fai cosa C            |\n";
+    std::cout << " ============================ \n";
+    std::cout << " | 0) Esci dal programma    |\n";
+    std::cout << " ============================ \n";
+    std::cout << "\n";
+    std::cout << "Scegli: ";
+
+
+}
+
+void loopMenu() {
+    int scelta;
+    do {
+        benvenuto();
+        displayMenu();
+        std::string s;
+        std::cin >> s;
+        scelta = std::stoi(s);
+
+        clear();
+
+        if (scelta < 0 || scelta>4) throw 1;
+    } while (scelta);
+}
+
+int main()
+{
+
+    try {
+        loopMenu();
+    }
+    catch (...) {
+        std::cerr << "ERRORE\n";
+        std::cerr << "Selezionare un numero valido\n";
+    }
+
+    /*
+    std::cout << " \nPress any key to continue\n";
+    std::cin.ignore();
+    std::cin.get();
+    */
+
+    return 0;
+}
